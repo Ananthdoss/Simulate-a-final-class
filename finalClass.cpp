f035814c03391b25d3ab8bbfe6f64c2ff43c40eb
Ananth S <ananthshankardoss@gmail.com>
2018-02-10 13:30:37 +0530

    Create finalClass.cpp

---
+++ finalClass.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ 
+class Final;  // The class to be made final
+ 
+class MakeFinal // used to make the Final class final
+{
+private:
+    MakeFinal() { cout << "MakFinal constructor" << endl; }
+friend class Final;
+};
+ 
+class Final : virtual MakeFinal
+{
+public:
+    Final() { cout << "Final constructor" << endl; }
+};
+ 
+class Derived : public Final // Compiler error
+{
+public:
+    Derived() { cout << "Derived constructor" << endl; }
+};
+ 
+int main(int argc, char *argv[])
+{
+	Final f;  // works
+    
+    return 0;
+}
